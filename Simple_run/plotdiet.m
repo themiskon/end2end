ad1a93b99e63d8dbecd9341dc32688732884b746
themiskon <79980335+themiskon@users.noreply.github.com>
2022-05-22 11:53:05 +0200

    Update plotdiet.m
    updated diet plots (bug fix)

--- Simple_run/plotdiet.m
+++ Simple_run/plotdiet.m
@@ -11,21 +11,10 @@ ymean=mean(y(Bin:end,:));
 
 [f, mortpred, Eavail] = calcEncounter(y(end,:)', param);
 
-bom1 = param.theta(param.ixFish(1):ystage,:) .* mean(y(Bin:end,:)); 
-bomdummy=bom1(:,param.ixFish(1):param.ixFish(end));
-
-for i=1:length(param.ixZoo)/10
-a(:,i)=sum(bom1(:,param.(sprintf('ixZoo%d',i))),2);
-end
-% b=zeros(length(a(:,1)),length(param.ixZoo)/10);
-b(:,1)=a(:,1);
-for i=2:length(param.ixZoo)/10
-b=[b , a(:,i)];
-end
-   bom=[b , bom1(:,param.ixBent), bomdummy];
-fbom = f(param.ixFish(1):ystage)' ./ sum(bom,2);
+bom=param.theta(param.ix1(1):ystage,:) .* mean(y(Bin:end,:)); 
+fbom = f(param.ix1(1):ystage)' ./ sum(bom,2);
 output = bom .* fbom;
-zoogroups=ones(1,length(param.ixZoo)/10);
+zoogroups=ones(1,length(param.ixZoo));
 colspec = [zoogroups 2 2 repmat(3,param.nstage*2/3,1)' ...
            repmat(4,param.nstage*2/3,1)'         ...
            repmat(5,param.nstage,1)'             ...
@@ -42,10 +31,10 @@ colorSet = [ 0      0.5      0;
 figure()
 
 small_pel = output(param.ix1(1)-param.ixR(end):param.ix2(1)-param.ixR(end),:);
-small_pel = [small_pel; zeros(ysmall,ystage-length(param.ixZoo)+length(param.ixZoo)/10)];
+small_pel = [small_pel; zeros(ysmall,ystage)];
 subplot(2,3,1)
 H = bar(small_pel, 'stacked');
- for i = 1:ystage-length(param.ixZoo)+2
+ for i = 1:ystage
      H(i).FaceColor = colorSet(colspec(i),:);
      H(i).LineStyle = 'none';
  end
@@ -56,10 +45,10 @@ set(gca,'XTick',[])
 xlabel('size-classes')
  
 meso_pel = output(param.ix1(2)-param.ixR(end):param.ix2(2)-param.ixR(end),:);
-meso_pel = [meso_pel; zeros(ysmall,ystage-length(param.ixZoo)+length(param.ixZoo)/10)];
+meso_pel = [meso_pel; zeros(ysmall,ystage)];
 subplot(2,3,2)
 H = bar(meso_pel, 'stacked');
- for i = 1:ystage-length(param.ixZoo)+2
+ for i = 1:ystage
      H(i).FaceColor = colorSet(colspec(i),:); 
      H(i).LineStyle = 'none';
  end
@@ -72,7 +61,7 @@ xlabel('size-classes')
 large_pel = output(param.ix1(3)-param.ixR(end):param.ix2(3)-param.ixR(end),:);
 subplot(2,3,3)
 H = bar(large_pel, 'stacked');
- for i = 1:ystage-length(param.ixZoo)+2
+ for i = 1:ystage
      H(i).FaceColor = colorSet(colspec(i),:);
      H(i).LineStyle = 'none';
  end 
@@ -85,7 +74,7 @@ xlabel('size-classes')
 bathy_pel = output(param.ix1(4)-param.ixR(end):param.ix2(4)-param.ixR(end),:);
 subplot(2,3,4)
 H = bar(bathy_pel, 'stacked');
- for i = 1:ystage-length(param.ixZoo)+2
+ for i = 1:ystage
      H(i).FaceColor = colorSet(colspec(i),:); 
      H(i).LineStyle = 'none';
  end 
@@ -98,7 +87,7 @@ xlabel('size-classes')
 demers = output(param.ix1(5)-param.ixR(end):param.ix2(5)-param.ixR(end),:);
 subplot(2,3,5)
 H = bar(demers, 'stacked');
- for i = 1:ystage-length(param.ixZoo)+2
+ for i = 1:ystage
      H(i).FaceColor = colorSet(colspec(i),:); 
      H(i).LineStyle = 'none';
  end 
@@ -111,5 +100,5 @@ xlabel('size-classes')
 % plot legend
 z=param.ixZoo(end)-3;
 ix=param.ix1;
-nspecies=[1 length(param.ixZoo)/10+1 length(param.ixZoo)/10+3 ix(2)-z ix(3)-z ix(4)-z ix(5)-z];
+nspecies=[1 param.ixBent(1) ix(1) ix(2) ix(3) ix(4) ix(5)];
 legend(H(nspecies),{'Zoopl','Benthos','Spel','Mpel','Lpel','Bpel','Ldem'})
